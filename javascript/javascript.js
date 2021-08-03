c376bfc4ff17b704fd0b0ca78530f020afe9bc37
AndersonF-Silva <anderson_fdasilva@hotmail.com>
2021-08-03 16:41:54 -0300

    Atualizações iniciais

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,1 +1,5 @@
-esteseasdfasfasdfsadfs
+function botao() {
+    document.getElementById("agradecimento").innerHTML = "Obrigado por Clicar";
+    /*console.log(document.getElementById("agradecimento"));*/
+    /*alert("Obrigado por Clicar!")*/
+};
